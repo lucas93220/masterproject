5be60da459b98e8d2859b17211d5f120645733d9
lucas93220 <91134554+lucas93220@users.noreply.github.com>
2025-06-15 16:39:11 +0200

    fichier app.js

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,35 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { sequelize } = require('./config/db');
+const errorHandler = require('./utils/errorHandler');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
-// Test de la connexion PostgreSQL
-sequelize.authenticate()
-  .then(() => console.log('Connecté à PostgreSQL'))
-  .catch(err => console.error('Erreur PostgreSQL :', err));
+const authRoutes = require('./routes/authRoutes');
+const utilisateurRoutes = require('./routes/utilisateurRoutes');
+const vetementRoutes = require('./routes/vetementRoutes');
+const categorieRoutes = require('./routes/categorieRoutes');
+const tenueRoutes = require('./routes/tenueRoutes');
+const evaluationRoutes = require('./routes/evaluationRoutes');
+const dressingRoutes = require('./routes/dressingRoutes');
+const photoRoutes = require('./routes/photoRoutes');
+const meteoRoutes = require('./routes/meteoRoutes');
 
+app.use('/api/auth', authRoutes);
+app.use('/api/utilisateur', utilisateurRoutes);
+app.use('/api/vetement', vetementRoutes);
+app.use('/api/categorie', categorieRoutes);
+app.use('/api/tenue', tenueRoutes);
+app.use('/api/evaluation', evaluationRoutes);
+app.use('/api/dressing', dressingRoutes);
+app.use('/api/photo', photoRoutes);
+app.use('/api/meteo', meteoRoutes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur le port ${PORT}`);
